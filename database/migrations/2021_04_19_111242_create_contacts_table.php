72a83882577ca35e2346e2d0b0399536d4c95310
teionhozonn <teionhozonn@gmail.com>
2021-04-20 03:55:43 +0900

    0420

--- database/migrations/2021_04_19_111242_create_contacts_table.php
+++ database/migrations/2021_04_19_111242_create_contacts_table.php
@@ -15,7 +15,22 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->unsignedBigInteger('condition_id');
+            $table->unsignedBigInteger('design_id');
+            $table->string('email');
+            $table->string('tel_number');
+            $table->string('fax_number');
+            $table->integer('zipcode');
+            $table->string('pref');
+            $table->string('city');
+            $table->string('street');
+            $table->string('surname');
+            $table->string('name');
+            $table->string('memo');
+            $table->string('private_memo');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(ContactsTableSeeder::class);
+        $this->call(ConditionsTableSeeder::class);
+        $this->call(DesignsTableSeeder::class);
     }
 }
